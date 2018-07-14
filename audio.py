ef641eae31714c3bf9b44e940d7afd2f689e1f7c
CharmingMother <nawhpongmaji1@gmail.com>
2018-07-14 05:02:17 -0500

    Update audio.py

--- audio.py
+++ audio.py
@@ -8,6 +8,26 @@ from discord.ext.commands import Bot
 
 bot=commands.Bot(command_prefix='a.')
 
+from discord import opus
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll',
+             'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
+
+def load_opus_lib(opus_libs=OPUS_LIBS):
+    if opus.is_loaded():
+        return True
+
+    for opus_lib in opus_libs:
+            try:
+                opus.load_opus(opus_lib)
+                return
+            except OSError:
+                pass
+
+    raise RuntimeError('Could not load an opus lib. Tried %s' %
+                       (', '.join(opus_libs)))
+load_opus_lib()
+
 
 
 @bot.event
