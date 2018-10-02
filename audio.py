f8d9c0db54e967031a834be7005b53b62987571f
CharmingMother <nawhpongmaji1@gmail.com>
2018-10-02 14:46:12 -0500

    Update audio.py

--- audio.py
+++ audio.py
@@ -90,7 +90,7 @@ async def stop(ctx):
 @bot.command(pass_context=True)
 async def var(con,msg):
   await bot.say(players)
-  await bot.say(players['251397550307803136']))
+  await bot.say(players['251397550307803136'])
   await bot.say(dir(players['251397550307803136']))
 
 
