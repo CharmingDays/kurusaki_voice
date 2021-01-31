a181e55defa9eab3c54f99d73296bdbfbaa3e80c
CharmingDays <nawhpongmaji1@gmail.com>
2021-01-31 05:54:05 -0600

    adding lavalink branch
    adding test version of lavalink

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+exts=['lava'] #Add your Cog extensions here
 
 
 def get_prefix(bot, msg):
@@ -20,7 +21,6 @@ bot=commands.Bot(command_prefix=get_prefix,description='Multipurpose Discord Bot
 
 
 
-exts=['music'] #Add your Cog extensions here
 
 
 @bot.event
@@ -28,15 +28,15 @@ async def on_ready():
     song_name='TWICE - What is love?'  #Status name
     activity_type=discord.ActivityType.listening #Status type
     await bot.change_presence(activity=discord.Activity(type=activity_type,name=song_name))
+
+    for i in exts:
+        bot.load_extension(i)
     print(bot.user.name)
 
 
 
 
 
-
-for i in exts:
-    bot.load_extension(i)
 
 
 bot.run(os.environ['TOKEN'])
